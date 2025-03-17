85dbff5bd2118c3f2bf6810c7e35dbcaf3980510
Magno Gomes <117935539+ongamse@users.noreply.github.com>
2025-03-17 11:13:02 -0400

    Fixing src/main/java/io/shiftleft/controller/SearchController.java for finding 13

--- src/main/java/io/shiftleft/controller/SearchController.java
+++ src/main/java/io/shiftleft/controller/SearchController.java
@@ -20,13 +20,18 @@ public class SearchController {
   @RequestMapping(value = "/search/user", method = RequestMethod.GET)
   public String doGetSearch(@RequestParam String foo, HttpServletResponse response, HttpServletRequest request) {
     java.lang.Object message = new Object();
-    try {
-      ExpressionParser parser = new SpelExpressionParser();
-      Expression exp = parser.parseExpression(foo);
-      message = (Object) exp.getValue();
-    } catch (Exception ex) {
-      System.out.println(ex.getMessage());
+[
+    {
+        "[java.util.logging.Logger]": "VALID and ACTIVE",
+        "[org.slf4j.LoggerFactory]": "VALID and ACTIVE",
+        "[ch.qos.logback.classic.PatternLayout]": "VALID and ACTIVE",
+        "[ch.qos.logback.core.AppenderBase]": "VALID and ACTIVE"
+    },
+    {
+        "[java.util.logging.Level]": "VALID and ACTIVE"
     }
+]
+
     return message.toString();
   }
 }
